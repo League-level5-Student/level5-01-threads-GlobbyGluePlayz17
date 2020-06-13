bb8e5eb7c929ec1b729384163a198219207faca2
GlobbyGluePlayz17 <dudisd888@gmail.com>
2020-06-13 15:00:34 -0700

    ThreadPool (X), SynchronizedSwimmer
    "Coding is Disneyland for the mind."

--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,8 +1,14 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
+	ConcurrentLinkedQueue taskQueue = new ConcurrentLinkedQueue<Task>();
 
+	Worker(taskQueue) {
+		
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
